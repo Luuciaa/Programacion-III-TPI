fe8ebb498d23feb304abc3fb56533853f57aef35
Lucia Dell Olio <122415757+Luuciaa@users.noreply.github.com>
2025-06-13 19:45:40 -0300

    modification

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
+import {PORT} from "./config"
 
 const app = express();
 
-app.listen(port);
+app.listen(PORT);
 app.use();
 
-console.log(`Server listening on port ${port}`);
+console.log(`Server listening on port ${PORT}`);
